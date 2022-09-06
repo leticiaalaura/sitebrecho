439a987cd2b77e952b39dfb93cd8927f7c08dacf
Letícia <leticiaalaura@gmail.com>
2022-09-06 15:05:14 -0300

    Produtos inseridos

--- base/app/views.py
+++ base/app/views.py
@@ -5,7 +5,9 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html', context={"produtos": [
-        {"nome": "camiseta rosa", "preco": 49.90, "descricao":"camisa rosa com gola"},]
+        {"produto": "Blazer listrado", "preco": 59.90, "descricao": "Blazer listrado com forro"},
+        {"produto": "Vestido menta", "preco": 49.90, "descricao": "Vestido cor menta e estampa Animal Print"},
+        {"produto": "Saia midi onça", "preco": 69.90, "descricao": "Saia midi tecido listrado estampa onça"},]
                                                  })
 
  # C:\Users\letic\PycharmProjects\sitebrecho\base\app\templates\homee.html
