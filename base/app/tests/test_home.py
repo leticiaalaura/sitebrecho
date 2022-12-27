f602ad5d637c24a820f50e89fec047622b75a5af
Letícia <leticiaalaura@gmail.com>
2022-12-27 19:27:21 -0300

    Testando home.html

--- base/app/tests/test_home.py
+++ base/app/tests/test_home.py
@@ -1,6 +1,32 @@
+from http import HTTPStatus
+
+import pytest
+from django.core.files.uploadedfile import SimpleUploadedFile
 from django.test import Client
+from pytest_django.asserts import assertTemplateUsed, assertContains
+
+from base.app.models import Produto
+
+@pytest.fixture(autouse=True)
+def enable_db_access_for_all_tests(db):
+    pass
+
+@pytest.fixture
+def resp(client):
+    Produto.objects.create(produto='blusa', preco=100000, descricao='vestido azul', foto=SimpleUploadedFile(name='test_image.jpg', content=b'', content_type='image/jpeg'))
+    return client.get('/')
+
+def test_status_code(resp):
+    assert resp.status_code == HTTPStatus.OK
+    assertTemplateUsed(resp, 'home.html')
+
+def test_template_content(resp):
+    assertContains(resp, 'vestido azul', 1)
+    assertContains(resp, 'blusa', 1)
+    assertContains(resp, '100000', 1)
+    assertContains(resp, '<img src="/media/test_image_', 1)
 
 
-def test_status_code(client:Client):
-    resp=client.get('/')
-    assert resp.status_code == 200
+
+
+
