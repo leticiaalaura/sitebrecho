07b6531c3cc18b889a5ec09e50dabea8b16571b3
leonardoaugusto <sleonardoaugusto@gmail.com>
2023-03-09 21:17:10 -0300

    livereload added

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # sitebrecho
+
 Site de vendas do Brechó Esse é Meu
+
+## Como instalar
+
+1. Clone o projeto
+2. Crie a virtualenv utilizando python > 3.7
+3. Ative a virtualenv
+4. Instale as dependencias
--- base/settings.py
+++ base/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'livereload',
     'django.contrib.staticfiles',
     'base.app',
 ]
@@ -46,6 +47,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'livereload.middleware.LiveReloadScript',
 ]
 
 ROOT_URLCONF = 'base.urls'
