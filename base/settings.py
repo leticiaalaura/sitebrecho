73bd274ed13788ea0fa805b94130b327fbc7d163
leonardoaugusto <sleonardoaugusto@gmail.com>
2023-03-09 22:07:39 -0300

    live reload removed

--- base/settings.py
+++ base/settings.py
@@ -34,7 +34,6 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'livereload',
     'django.contrib.staticfiles',
     'base.app',
 ]
@@ -47,7 +46,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'livereload.middleware.LiveReloadScript',
 ]
 
 ROOT_URLCONF = 'base.urls'
